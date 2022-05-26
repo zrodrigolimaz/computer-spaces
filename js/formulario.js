e0ee29c5168192f89643dc86266f1e411beaa700
zrodrigolimaz <zrodrigolimaz@gmail.com>
2022-05-26 00:15:59 -0300

    updates

---
+++ js/formulario.js
@@ -0,0 +1,27 @@
+const formulario = document.querySelector("form");
+
+function formularioEnviado(resposta) {
+  if (resposta.ok) {
+    formulario.innerHTML =
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #317A00;'>Mensagem enviada</span>, em breve entrarei em contato. Geralmente respondo em 24h. Precisa de uma reposta rápida? fale comigo pelo Linkedin! :) </p>";
+  } else {
+    formulario.innerHTML =
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #E00000;'>Erro ao enviar! Tente novamente.</span> Precisa de uma reposta rápida? fale comigo pelo Linkedin! </p>";
+  }
+}
+
+function enviarFormulario(event) {
+  event.preventDefault();
+  const botao = document.querySelector("form button");
+  botao.disabled = true;
+  botao.innerText = "Enviando...";
+
+  const data = new FormData(formulario);
+
+  fetch("./enviar.php", {
+    method: "POST",
+    body: data,
+  }).then(formularioEnviado);
+}
+
+formulario.addEventListener("submit", enviarFormulario);
